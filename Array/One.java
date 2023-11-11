ed35517b3bfad01f892209fdf5656f848e862039
VishwajeePatil <techtour1620@gmail.com>
2023-11-11 08:27:49 +0530

    Arrays

---
+++ Array/One.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class One {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        // type[] arr_name = new type[size]
+        // int[] marks = new int[4];
+        // marks[0] = 16;
+        // marks[1] = 17;
+        // marks[2] = 15;
+        // marks[3] = 18;
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        // System.out.println(marks[3]);
+
+
+        // type[] arr_name = {values}
+        // int[] marks = {16,17,15,18};
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        // System.out.println(marks[3]);
+
+
+        // System.out.println("Enter The Size Of Array : ");
+        // int size = sc.nextInt();
+        // int[] arr = new int[size];
+        // for(int i=0; i<size; i++){
+        //     arr[i] = sc.nextInt();
+        // }
+        // for(int i=0; i<arr.length; i++){
+        //     System.out.println(arr[i]);
+        // }
+    }
+}
